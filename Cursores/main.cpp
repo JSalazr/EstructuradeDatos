e0b054e882cfc172032d1f52711fee0cf482873c
JSalazr <jonathansalazar96@gmail.com>
2015-10-27 10:44:33 -0600

    cursores

--- Cursores/main.cpp
+++ Cursores/main.cpp
@@ -5,56 +5,95 @@ using namespace std;
 
 class Nodo{
 public:
-    int valor;
+    char valor;
     int next;
-    Nodo(int valor){
-        this->valor=valor;
-        next=-1;
-    }
 };
 
-int m=-1;
-int n=-1;
+int cont=0;
 int disp=0;
 const int tam=10;
-Nodo* arreglo[tam];
+Nodo arreglo[tam];
+int listas[10];
 
-int nextDisp(){
+int nextList(){
     for(int cont=0; cont<tam; cont++){
-        if(arreglo[cont]->valor==-1){
-            disp=cont;
-            return disp;
+        if(listas[cont]==-1){
+            return cont;
         }
     }
     return -1;
 }
 
-bool agregar(int valor, int lista){
-    if(disp==-1)
-        return false;
-    if(lista==-1){
-        lista=disp;
-        arreglo[disp]->valor=valor;
-        arreglo[disp]->next=nextDisp();
-        return true;
+int crear(char valor){
+    if(cont<10){
+        int nextDisp=listas[0];
+        arreglo[nextDisp].valor=valor;
+        listas[0]=arreglo[nextDisp].next;
+        arreglo[nextDisp].next=-1;
+        listas[nextList()]=nextDisp;
+        cont++;
     }
-    else{
-        for(int cont=0; cont<tam; cont++){
-            if(arreglo[cont]->valor!=-1 && arreglo[cont]->next==-1){
-                arreglo[disp]->valor=valor;
-                arreglo[disp]->next=nextDisp();
-                return true;
-            }
+}
+
+void agregar(char valor, int lista){
+    if(cont<10){
+        int nextDisp=listas[0];
+        Nodo* temp=&arreglo[listas[lista]];
+        while(temp->next!=-1){
+            temp=&arreglo[temp->next];
         }
+        temp->next=nextDisp;
+        arreglo[nextDisp].valor=valor;
+        listas[0]=arreglo[nextDisp].next;
+        arreglo[nextDisp].next=-1;
+        cont++;
+    }
+}
+
+int buscar(char valor, int lista){
+    Nodo* temp=&arreglo[listas[lista]];
+    while(temp->next!=-1){
+        if(arreglo[temp->next].valor==valor)
+            return temp->next;
+        temp=&arreglo[temp->next];
     }
     return false;
 }
 
+void insertar(char valor, int lista, char despuesDe){
+    if(cont<10){
+        int nextDisp=listas[0];
+        Nodo* temp=&arreglo[listas[lista]];
+        while(temp->next!=-1){
+            if(temp->valor==despuesDe){
+                arreglo[nextDisp].valor=valor;
+                listas[0]=arreglo[nextDisp].next;
+                arreglo[nextDisp].next=temp->next;
+                temp->next=nextDisp;
+                cont++;
+            }
+            temp=&arreglo[temp->next];
+        }
+    }
+}
+
+void eliminar(char valor, int lista){
+    Nodo* temp=&arreglo[listas[lista]];
+    while(temp->next!=-1){
+        if(arreglo[temp->next].valor==valor){
+            listas[0]=temp->next;
+            temp->next=arreglo[temp->next].next;
+            cont--;
+        }
+        temp=&arreglo[temp->next];
+    }
+}
+
 void print(int lista){
-    int cont=lista;
-    while(cont!=-1){
-        cout<<arreglo[cont]<<endl;
-        cont=arreglo[cont]->next;
+    Nodo* temp=&arreglo[listas[lista]];
+    while(temp->valor!=NULL){
+        cout<<temp->valor<<endl;
+        temp=&arreglo[temp->next];
     }
 }
 
@@ -62,32 +101,49 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     int opc;
-    int valor;
+    char valor;
     int lista;
+    char despuesDe;
     for(int cont=0; cont<tam; cont++){
-        arreglo[cont]=new Nodo(-1);
+        arreglo[cont].next=cont+1;
     }
+    for(int cont=0; cont<10; cont++){
+        listas[cont]=-1;
+    }
+    listas[0]=0;
+    arreglo[9].next=-1;
+
     while(true){
-        cout<<"1-Agregar"<<endl;
-        cout<<"2-Buscar"<<endl;
-        cout<<"3-Eliminar"<<endl;
-        cout<<"4-Insertar"<<endl;
+        cout<<"1-Crear"<<endl;
+        cout<<"2-Agregar"<<endl;
+        cout<<"3-Buscar"<<endl;
+        cout<<"4-Eliminar"<<endl;
+        cout<<"5-Insertar"<<endl;
         cin>>opc;
         switch(opc){
             case 1:
                 cin>>valor;
+                crear(valor);break;
+            case 2:
+                cin>>valor;
                 cin>>lista;
-                if(lista==1)
-                    agregar(valor, m); break;
-                if(lista==2)
-                    agregar(valor, n); break;
+                agregar(valor,lista);break;
+            case 3:
+                cin>>valor;
+                cin>>lista;
+                cout<<buscar(valor, lista)<<endl;break;
+            case 4:
+                cin>>valor;
+                cin>>lista;
+                eliminar(valor, lista);break;
             case 5:
+                cin>>valor;
                 cin>>lista;
-                if(lista==1)
-                    print(m); break;
-                if(lista==2)
-                    print(n); break;
-
+                cin>>despuesDe;
+                insertar(valor, lista, despuesDe);break;
+            case 6:
+                cin>>lista;\
+                print(lista);
         }
     }
     return a.exec();
